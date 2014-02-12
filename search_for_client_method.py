7e13a2436c511211a80a237700c4af8d5eec7b31
fpdev <fpdev@gouda.(none)>
2014-02-12 17:10:31 +0000

    TF updated to use HOME dir not hardcoded

--- search_for_client_method.py
+++ search_for_client_method.py
@@ -5,19 +5,21 @@
 
 import sys, os 
 
-sys.path.append('/home/ad/client_search/app/searcher')
-sys.path.append('/home/ad/client_search/app')
-#print sys.path
-import searcher
-import app
-
 if len(sys.argv) != 3:
     print '2 arguments are required, the client first and then the client method.' 
     exit()
 
+HOME = os.getenv("HOME")
 CURRENT_DIRECTORY = os.getcwd()
 TARGET_CLIENT = sys.argv[1]
 TARGET_METHOD = sys.argv[2]
+
+#move the following into an initialiser, i.e. to append all paths to PYTHONPATH
+sys.path.append(HOME+'/client_search/app/searcher')
+sys.path.append(HOME+'/client_search/app')
+#print sys.path
+import searcher
+import app
 
 client_search_params = {'root_dir': CURRENT_DIRECTORY, 
                         'target_client': TARGET_CLIENT, 
